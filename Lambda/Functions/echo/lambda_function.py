d0b0c42652e05ed413844051f2576888beb4edfc
Andrew R. DeFilippis <defila@amazon.com>
2016-12-30 16:55:22 -0800

    Commit echo function

---
+++ Lambda/Functions/echo/lambda_function.py
@@ -0,0 +1,18 @@
+"""Created by: Andrew R. DeFilippis"""
+
+# Copyright 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
+# except in compliance with the License. A copy of the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed on an "AS IS"
+# BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under the License.
+
+
+def lambda_handler(event, context):
+    """Respond with the ingested event to echo back what is received.
+    """
+    return event
