e231ef21591736213aaf9793d6e6a4c44a9456db
Rubem de Lima Savordelli <rsavordelli@gmail.com>
2017-05-02 07:33:17 +0200

    Added SESReports

---
+++ SES/SESReports/README.md
@@ -0,0 +1,132 @@
+# Amazon - AWS Simple Email Service | SES | Reports
+TODO: This is a lambda-function that generate reports of Bounce and Complaint and save on S3 Bucket.
+</br>
+</br>
+However, the logic to control the Bounce and Complaints notifications is ready to be customized.
+<br />
+The reports looks like:[NotificationType;destination;subtype(ifExist);DiagnosticCode(IfExist);TimeStamp]
+<br />
+ ```
+Type	Subtype	Detail	Email	Error	Date	MessageID
+Bounce	Permanent	General	bounce@simulator.amazonses.com	smtp; 550 5.1.1 user unknown	2017-03-08T13:30:11.000Z	0102015aae1cde78-2b30806b-7932-425f-bfa3-ac0eca7eebda-000000
+Bounce	Permanent	General	bounce@simulator.amazonses.com	smtp; 550 5.1.1 user unknown	2017-03-08T13:30:52.000Z	0102015aae1d7d9f-3b290f2a-1741-4ff2-bac7-6781a1117894-000000
+Bounce	Permanent	Suppressed	otoo@simulator.amazonses.com	Suppressed by SES	2017-03-08T13:25:46.782Z	0102015aae18d17a-0976a238-c24d-4411-9fd0-7874229c1f9b-000000
+```
+
+I'd like to encourage you to use the Mailbox Simulator provided by SES when deploying this script. It will avoid unnecessary bounces in your account.
+
+## Environment details:
+* SES Account up and running 
+* SNS Notification for Bounces and Complaints || Notifications
+* SQS || Manage the Notification's queue
+* CloudWatch Events || Will trigger the Lambda function
+* Lambda NodeJS || Will generate the report and send the URL via Email(SES)
+
+## How does it work?
+*	Amazon SES
+*	Amazon SNS – Bounces and Complaints notifications
+*	Amazon SQS – Queue for the Notifications
+*	Lambda – Will Process the queue, create the Report, save to S3 and send the report URL via Email using SES
+*	CloudWatch – Will trigger the lambda-function using “Scheduled Events"
+
+
+## Installation:
+*	Create an Amazon SNS topic [1].
+*	Create an Amazon SQS Standard Queue [2]. Only changing: Default Visibility Timeout to 5m. 
+*	Subscribe the queue to SNS Topic previously created [3].
+*	Create an Amazon S3 Bucket.
+*	Create an IAM policy(Create your Own Policy) with the following permission (changing the Queue ARN for yours and the BucketName) [4].
+
+ ```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1487170767000",
+            "Effect": "Allow",
+            "Action": [
+                "ses:SendEmail"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+         {
+            "Sid": "s3allow",
+            "Effect": "Allow",
+            "Action": [
+                 "s3:PutObject",
+                 "s3:PutObjectAcl"
+            ],
+            "Resource": [
+                "arn:aws:s3:::YourBucketName/*"
+            ]
+        },
+        {
+            "Sid": "Stmt1487170835000",
+            "Effect": "Allow",
+            "Action": [
+                "sqs:ChangeMessageVisibility",
+                "sqs:ChangeMessageVisibilityBatch",
+                "sqs:DeleteMessage",
+                "sqs:DeleteMessageBatch",
+                "sqs:GetQueueAttributes",
+                "sqs:GetQueueUrl",
+                "sqs:ReceiveMessage"
+            ],
+            "Resource": [
+                "arn:aws:sqs:REGION:ACCOUNT-ID:QueueName"
+            ]
+        }
+    ]
+}
+
+ ```
+*	Create an IAM Role and attach the policy just created (You will use this Role for lambda execution) [5].
+*	Create an Amazon lambda-function following the steps below.
+
+## Creating Lambda Function
+Firstly compress(zip) the folder lamba-dep-package’s content (the content, not the folder) which you can find on the “The code” session of this manual.
+*  Sign in to the AWS Management Console and open the AWS Lambda console.
+* Choose Get Started Now.
+* Create a Lambda function go to the Lambda > New function page. 
+* Select the Runtime Node.js 4.3
+* Choose Black Function
+* On Triggers, click on Next (We will do this step later)
+* Give a Name and description for your Lambda-Function
+* On “Code entry Type” chose “Upload a .ZIP file”, select the zip file created from “lamba-dep-package” CONTENT, not the folder.
+* On Environment variables, you need to add the following variables:
+```
+Region: eu-west-1 // Your Queue Region [REQUIRED]
+QueueURL: https://sqs.eu-west-1.am… // The Queue URL [REQUIRED]
+ToAddr: yourVerifiedEmail // Email address which will receive the report [Optional – If you don’t create it, no report will be sent via Email]
+SrcAddr: youVerifiedEmail // Email address which will send the report [REQUIRED IF using ToAddr]
+BucketName: yourBucketName  // Bucket to save the report [REQUIRED]
+BucketPrefix: YourPrefix/ // Bucket prefix to save the report [Optional – If present, need to finish with slash “/”]
+```
+
+* On role: Select the “Chosen an existing role”> Select the role previously created
+* Set the memory  (Advanced tab) to 512MB if less 5000 bounces/complaints per day, 1024MB if more than 5000 or 1536MB if more than 10000.
+* Set the Timeout for 5 minutes  
+* Click on Next > Review your information and “Create function”
+
+## Enabling the Bounces and Complaints Notifications for your identities 
+*	Go to SES Console
+*	Edit the notification for the identity
+*	Select the SNS topic created for Bounces and Complaints
+*	Then go to SQS Console > Select the Queue > Purge Queue (This is a required step because SES sends a confirmation notification and it’s not needed).
+
+## Triggering the Function with CloudWatch Schedule Events
+*	Go to CloudWatch console
+*	Click on Events > Rules > Create Rule
+*	Select Schedule > Fixed rate > 1 Day
+*	On Target, select the lambda-function > Configure Details
+*	Give a name > Create Rule
+
+DOC Refference: 
+[1] Creating SNS Topic: http://docs.aws.amazon.com/sns/latest/dg/CreateTopic.html
+[2] Creating SQS Standard Queue: http://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-create-queue.html
+[3] Subscribing a Queue to SNS Topic: http://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-subscribe-queue-sns-topic.html
+[4] Creating IAM Policy: http://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies_create.html#access_policies_create-editor
+[5] Creating IAM Role (AWS Lambda Role): http://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_create_for-service.html 
+[6] CloudWatch Event Schedule: http://docs.aws.amazon.com/AmazonCloudWatch/latest/events/RunLambdaSchedule.html#schedule-create-rule
---
+++ SES/SESReports/lambda-dep-package/index.js
@@ -0,0 +1,219 @@
+var AWS = require('aws-sdk');
+var ses = new AWS.SES({ apiVersion: '2010-12-01' });
+var sqs = new AWS.SQS({ region: process.env.Region, httpOptions: { agent: agent } });
+var s3 = new AWS.S3();
+var https = require('https');
+var agent = new https.Agent({ maxSockets: 150 });
+var fs = require('fs');
+var queueURL = process.env.QueueURL;
+var toddresses = process.env.ToAddr;
+var srcaddr = process.env.SrcAddr;
+var bucket = process.env.BucketName;
+var prefix = process.env.BucketPrefix;
+var qSize = null;
+var content = null;
+var queueParams = {AttributeNames: ["ApproximateNumberOfMessages"], QueueUrl: queueURL};
+
+
+exports.handler = (event, context, callback) => {
+    var date = (new Date()).toString().split(' ').splice(1, 4).join('-');
+    var url = null;
+
+    function s3upload() {
+        if (prefix == undefined) {
+            prefix = "";
+        }
+        var param = {
+            Bucket: bucket,
+            Key: prefix + date + ".html",
+            Body: content,
+            ACL: 'public-read',
+            ContentType: "text/html"
+        };
+        s3.upload(param, function (err, data) {
+            if (err) console.log(err, err.stack); // an error occurred
+            else console.log(data);
+            url = data.Location;
+            console.log("uploading to s3");
+            if (toddresses) {
+                sendMail();
+            }
+            //context.done();
+        });
+    }
+
+    function sendMail() {
+        //console.log("message: " +  messages);
+        var params = {
+            Destination: {
+                ToAddresses: [toddresses,]
+            },
+            Message: {
+                Body: {
+                    Html: {
+                        Data: url,
+                        Charset: 'utf-8'
+                    },
+                    Text: {
+                        Data: "report",
+                        Charset: 'utf-8'
+                    }
+                },
+                Subject: {
+                    Data: "[SES] Daily -  Notification Reports",
+                    Charset: 'utf-8'
+                }
+            },
+            Source: srcaddr,
+        };
+        ses.sendEmail(params, function (err, data) {
+            if (err) console.log(err, err.stack);
+            else console.log(data);
+            console.log("sending email");
+            context.done();
+        });
+    }
+
+    function initializeQueue(callbackQueue) {
+        console.log("Reading from: " + queueURL);
+        sqs.getQueueAttributes(queueParams, (err, data) => {
+            if (err) {
+                console.log("Possible issue with SQS permissions or QueueURL wrong")
+                callbackQueue(err, null);
+            } 
+            qSize = data.Attributes.ApproximateNumberOfMessages;
+            callbackQueue(null, qSize);
+        });
+    }
+
+    function deleteMessage(message) {
+        sqs.deleteMessage({
+            QueueUrl: queueURL,
+            ReceiptHandle: message.ReceiptHandle
+        }, (err, data) => {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            // console.log("Data removed. Response = " + data);  
+        });
+    }
+
+    //Start Receive message
+    initializeQueue((err, queueSize) => {
+        console.log("Reading queue, size = " + queueSize);
+
+        if (queueSize == 0) {
+            callback(null, 'Queue is empty.');
+        }
+
+        var messages = [];
+        var msgBouncePerm = [];
+        var msgSuppres = [];
+        var msgBounceTrans = [];
+        var msgComplaint = [];
+
+        for (var i = 0; i < queueSize; i++) {
+            sqs.receiveMessage(queueParams, (err, data) => {
+                if (err) {
+                    console.log(err, err.stack);
+                    throw err;
+                }
+
+                // console.log("data with message = " + data.Messages);
+                if (data.Messages) {
+                    var message = data.Messages[0];
+                    body = JSON.parse(message.Body);
+                    msg = JSON.parse(body.Message);
+                    var destination = msg.mail.destination[0];
+                    var type = msg.notificationType;
+                    var time = msg.mail.timestamp;
+                    var id = msg.mail.messageId;
+                    var otr = "<tr>";
+                    var ftr = "</tr>";
+                    var oline = "<td>";
+                    var cline = "</td>";
+                    var btype = null;
+                    var bsubtype = null;
+                    var diagcode = null;
+
+                    //console.log(msg);
+
+                    if (type == "Bounce") {
+                        btype = msg.bounce.bounceType; // Permanent || Transient
+                        bsubtype = msg.bounce.bounceSubType; // General || Supressed
+                        if (btype == "Permanent" && bsubtype == "Suppressed") {
+                            diagcode = "Suppressed by SES";
+                            text = otr + oline + type + cline + oline + btype + cline + oline + bsubtype + cline + oline + destination + cline + oline + diagcode + cline + oline + time + cline + oline + id + cline + ftr;
+                            msgSuppres.push(text);
+
+                        } else if (btype == "Permanent" && bsubtype == "General") {
+                            diagcode = msg.bounce.bouncedRecipients[0].diagnosticCode;
+                            text = otr + oline + type + cline + oline + btype + cline + oline + bsubtype + cline + oline + destination + cline + oline + diagcode + cline + oline + time + cline + oline + id + cline + ftr;
+                            msgBouncePerm.push(text);
+
+                        } else if (btype == "Permanent" && bsubtype == "NoEmail") {
+                            diagcode = msg.bounce.bouncedRecipients[0].diagnosticCode;
+                            text = otr + oline + type + cline + oline + btype + cline + oline + bsubtype + cline + oline + destination + cline + oline + diagcode + cline + oline + time + cline + oline + id + cline + ftr;
+                            msgBouncePerm.push(text);
+
+                        } else if (btype == "Undetermined") {
+                            diagcode = msg.bounce.bouncedRecipients[0].diagnosticCode;
+                            text = otr + oline + type + cline + oline + btype + cline + oline + bsubtype + cline + oline + destination + cline + oline + diagcode + cline + oline + time + cline + oline + id + cline + ftr;
+                            msgBouncePerm.push(text);
+
+                        } else if (btype == "Transient") {
+                            diagcode = "soft-Bounce";
+                            text = otr + oline + type + cline + oline + btype + cline + oline + bsubtype + cline + oline + destination + cline + oline + diagcode + cline + oline + time + cline + oline + id + cline + ftr;
+                            msgBounceTrans.push(text);
+
+                        } else {
+                            console.log("it's an unknown bounce");
+                            diagcode = "unknown";
+                            text = otr + oline + type + cline + oline + btype + cline + oline + bsubtype + cline + oline + destination + cline + oline + diagcode + cline + oline + time + cline + oline + id + cline + ftr;
+                            msgBouncePerm.push(text);
+                        }
+
+                    } else if (type == "Delivery") {
+                        console.log("Delivery notification not supported");
+
+                    } else if (type == "Complaint") {
+                        btype = "null";
+                        bsubtype = "null";
+                        diagcode = "null";
+                        text = otr + oline + type + cline + oline + btype + cline + oline + bsubtype + cline + oline + destination + cline + oline + diagcode + cline + oline + time + cline + oline + id + cline + ftr;
+
+                        msgComplaint.push(text);
+
+                    }
+                    
+                    else {
+                        console.log("not identified");
+                    }
+
+                    messages.push(i);
+
+                    deleteMessage(message);
+                    //console.log("Array size = " + messages.length + " with queue size = " + queueSize);
+
+                    if (messages.length == queueSize) {
+
+                        var bp = msgBouncePerm.join('');
+                        var sp = msgSuppres.join('');
+                        var bt = msgBounceTrans.join('');
+                        var cp = msgComplaint.join('');
+                        var begin = fs.readFileSync('template/begin.html', 'utf8');
+                        var middle = bp + sp + bt + cp;
+                        var end = fs.readFileSync('template/end.html', 'utf8');
+                        content = begin + middle + end;
+
+                        s3upload();
+
+                    }
+                } else {
+                    console.log("data without messages.");
+                }
+            });
+        }
+    });
+};
