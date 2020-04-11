2b40716b408a57e65f64347bbd3d7f914c75a852
Nambiar <gautagan@8c85908fa2f3.ant.amazon.com>
2020-04-10 18:47:26 -0700

    Review changes

--- Cognito/identity-pool-integrator-onelogin/identitypool_integrator_onelogin.py
+++ Cognito/identity-pool-integrator-onelogin/identitypool_integrator_onelogin.py
@@ -1,3 +1,4 @@
+"""
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this
@@ -12,7 +13,7 @@ PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIG
 HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+"""
 
 import requests
 import boto3
