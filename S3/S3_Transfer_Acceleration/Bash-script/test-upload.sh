bc3609cc8481b80e6ac7b9a579189391ef3376d9
Score_Under <seejay.11@gmail.com>
2020-04-23 21:39:00 +0100

    Tidy test-upload script

--- S3/S3_Transfer_Acceleration/Bash-script/test-upload.sh
+++ S3/S3_Transfer_Acceleration/Bash-script/test-upload.sh
@@ -13,104 +13,52 @@ BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
 License for the specific language governing permissions and limitations under the License.
 END
 
-# Error handling of the script.This comamnd makes sure that the script will stop and exit if there is an error.
 set -e
 
+readyn() {
+    local input=
+    while true; do
+        read -p "$* " input
+        case $input in
+            [yY]) echo true; return;;
+            [nN]) echo false; return;;
+            *) echo 'Invalid input. Valid inputs are "y" or "n".' >&2;;
+        esac
+    done
+}
+
+calc() {
+    printf %s\\n "$*" | bc
+}
+
+
 # Taking Input parameters from the user
-echo -n "Enter the local path of the file you want to upload: "
-read FILENAME
-echo -n "Enter the destination file name: "
-read S3_FILENAME
-echo -n "Enter the name of the bucket:"
-read bucketname
-echo -n "Enter the region of your Bucket (e.g. us-west-2): "
-read region
-i=1
-while [ $i -gt 0 ]
-do
-    echo -n "Do you want to upload via Transfer Acceleration: (y/n) "
-    read input
+read -p "Enter the local path of the file you want to upload: " FILENAME
+read -p "Enter the destination file name: " S3_FILENAME
+read -p "Enter the name of the bucket: " bucketname
+read -p "Enter the region of your Bucket (e.g. us-west-2): " region
 
-    if [ $input == "y" ] || [ $input == "n" ]
+use_acceleration=$(readyn 'Do you want to upload via Transfer Acceleration: (y/n)')
+
+FILESIZE="$(wc -c <"$FILENAME")"
+STARTTIME=$(date +"%s.%N")
+if $use_acceleration; then
+    # Uploading the file to the S3 Bucket using Transfer Acceleration.
+    use_acceleration_str="with acceleration"
+    aws configure set s3.addressing_style virtual
+    aws s3 cp "$FILENAME" "s3://$bucketname/$S3_FILENAME" --region "$region" --endpoint-url http://s3-accelerate.amazonaws.com
+else
     # Uploading the file to the S3 Bucket via a Direct upload.
-    then
-        if [ $input == "n" ]
-        then
-            FILESIZE="$(wc -c <"$FILENAME")"
-            STARTTIME=$(date +"%s.%N")
-            #echo "start-time-1:$STARTTIME"
-            STARTTIME_us=$(expr $STARTTIME*$((1000000)) | bc -l)
-            #echo "start-time-2:$STARTTIME_us"
-            aws s3 cp $FILENAME s3://$bucketname/$S3_FILENAME --region $region
-            ENDTIME=$(date +"%s.%N")
-            #echo "end-time-1:$ENDTIME"
-            ENDTIME_us=$(expr $ENDTIME*$((1000000)) | bc -l)
-            #echo "end-time-2:$ENDTIME_us"
-            Time_elapsed=$(expr $ENDTIME_us-$STARTTIME_us | bc -l)
-            #echo "Time_elapsed:$Time_elapsed"
-            Throughput=$(expr $(($FILESIZE*8))/$Time_elapsed | bc -l)
-            Throughput_in_Mbps=$(expr $Throughput/$((1024*1024)) | bc -l)
-            echo "File uploaded without acceleration at $(expr $Throughput_in_Mbps*$((1000000)) | bc -l) Mbps speed."
-            i=$(( i-1 ))
-            j=1
-            # Taking user input to check whether the user wants to delete the uploaded file.
-            while [ $j -gt 0 ]
-            do
-                echo -n "Do you want to delete the uploaded file: (y/n) "
-                read input_1
+    use_acceleration_str="without acceleration"
+    aws s3 cp "$FILENAME" "s3://$bucketname/$S3_FILENAME" --region "$region"
+fi
+ENDTIME=$(date +"%s.%N")
+Time_elapsed=$(calc "$ENDTIME-$STARTTIME")
+Throughput=$(calc "$FILESIZE*8/$Time_elapsed")
+Throughput_in_Mbps=$(calc "$Throughput/(1024*1024)")
+echo "File uploaded $use_acceleration_str at $Throughput_in_Mbps Mbps speed."
 
-                if [ $input_1 == "y" ] || [ $input_1 == "n" ]
-                then
-                    if [[ $input_1 == "y"  ]]; then
-                        aws s3 rm s3://$bucketname/$S3_FILENAME --region $region
-                        j=$(( j-1 ))
-                    else
-                        j=$(( j-1 ))
-                    fi
-                else
-                    echo "Invalid input.Valid inputs are 'y' or 'n'."
-                fi
-            done
-
-
-
-        # Uploading the file to the S3 Bucket via a Transfer Acceleration.
-        else
-            FILESIZE="$(wc -c <"$FILENAME")"
-            STARTTIME=$(date +"%s.%N")
-            STARTTIME_us=$(expr $STARTTIME*$((1000000)) | bc -l)
-            aws configure set s3.addressing_style virtual
-            aws s3 cp $FILENAME s3://$bucketname/$S3_FILENAME --region $region --endpoint-url http://s3-accelerate.amazonaws.com
-            ENDTIME=$(date +"%s.%N")
-            ENDTIME_us=$(expr $ENDTIME*$((1000000)) | bc -l)
-            Time_elapsed=$(expr $ENDTIME_us-$STARTTIME_us | bc -l)
-            Throughput=$(expr $(($FILESIZE*8))/$Time_elapsed | bc -l)
-            Throughput_in_Mbps=$(expr $Throughput/$((1024*1024)) | bc -l)
-            echo "File uploaded with acceleration at $(expr $Throughput_in_Mbps*$((1000000)) | bc -l) Mbps speed."
-            i=$(( i-1 ))
-            k=1
-            # Taking user input to check whether the user wants to delete the uploaded file.
-            while [ $k -gt 0 ]
-            do
-                echo -n "Do you want to delete the uploaded file: (y/n) "
-                read input_1
-
-                if [ $input_1 == "y" ] || [ $input_1 == "n" ]
-                then
-                    if [[ $input_1 == "y"  ]]; then
-                        aws s3 rm s3://$bucketname/$S3_FILENAME --region $region
-                        k=$(( k-1 ))
-                    else
-                        k=$(( k-1 ))
-                    fi
-                else
-                    echo "Invalid input.Valid inputs are 'y' or 'n'."
-                fi
-            done
-
-
-        fi
-    else
-        echo "Invalid input.Valid inputs are 'y' or 'n'."
-    fi
-done
+delete_file=$(readyn 'Do you want to delete the uploaded file: (y/n)')
+if $delete_file; then
+    aws s3 rm "s3://$bucketname/$S3_FILENAME" --region "$region"
+fi
