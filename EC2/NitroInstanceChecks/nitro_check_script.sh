0c65a28c082745187a7d314c2409ab3b958ceeb6
iitggithub <iitg182@gmail.com>
2022-10-11 05:43:44 +0000

    changed path to grub file for c6/r6/amzn1 to /boot/grub/grub.conf and /boot/grub/menu.lst respectively

--- EC2/NitroInstanceChecks/nitro_check_script.sh
+++ EC2/NitroInstanceChecks/nitro_check_script.sh
@@ -81,9 +81,10 @@ check_nvme_timeout () {
 
     # Check if Operating system is RHEL6, C6, Amazon Linux 1 etc
     # and set the correct path to grub configuration file
-    # These are often soft linked to /boot/grub/grub.conf or /boot/grub/menu.lst
-    if [ -n "`uname -r 2>/dev/null | grep -Eo '\.(amzn1|el6)\.' 2>/dev/null`" ]; then
-        grub_config_file="/etc/grub.conf"
+    if [ -n "`uname -r 2>/dev/null | grep -Eo '\.el6\.' 2>/dev/null`" ]; then
+        grub_config_file="/boot/grub/grub.conf"
+    elif [ -n "`uname -r 2>/dev/null | grep -Eo '\.amzn1\.' 2>/dev/null`" ]; then
+        grub_config_file="/boot/grub/menu.lst"
     fi
 
     # Check if NVMe io_timeout already configured in grub configuration
